295ae0c95cd27e9c763df5850d070b426613eda5
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-11-04 13:16:38 -0300

    Alterações sugeridas 1

--- spec/system/users/user_signs_in_spec.rb
+++ spec/system/users/user_signs_in_spec.rb
@@ -4,10 +4,8 @@ describe 'Usuário faz login' do
 
   context 'como administrador' do
     it 'com sucesso' do
-      # Arrange
       User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password', role: :admin)
-  
-      # Act
+
       visit root_path
       within 'nav' do
         click_on 'Entrar'
@@ -18,14 +16,9 @@ describe 'Usuário faz login' do
         click_on 'Entrar'
       end
   
-      # Assert
       within 'nav' do
         expect(page).not_to have_link 'Entrar'
-        expect(page).to have_link 'Seguradoras'
-        expect(page).to have_link 'Coberturas'
-        expect(page).to have_link 'Serviços'
         expect(page).to have_link 'Produtos'
-        expect(page).to have_link 'Categorias de Produtos'
         expect(page).to have_button 'Sair'
         expect(page).to have_content 'Pessoa'
       end
@@ -33,22 +26,16 @@ describe 'Usuário faz login' do
     end
 
     it 'e faz logout' do
-      # Arrange
       admin = User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password', role: :admin)
   
-      # Act
       login_as admin
       visit root_path
       click_on 'Pessoa'
       click_on 'Sair'
   
-      # Assert
       expect(page).to have_link 'Entrar'
       expect(page).not_to have_link 'Sair'
-      expect(page).not_to have_link 'Coberturas'
-      expect(page).not_to have_link 'Serviços'
       expect(page).not_to have_link 'Produtos'
-      expect(page).not_to have_link 'Categorias de Produtos'
       expect(page).not_to have_button 'Sair'
       expect(page).not_to have_button 'Pessoa'
       expect(page).to have_content 'Logout efetuado com sucesso.'
