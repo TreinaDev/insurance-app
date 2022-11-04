6071f8f197d97c760cfd7a5f28c0171059bca7d8
João Almeida <joao@campuscode.com.br>
2022-11-04 16:49:49 -0300

    Corrige erro ao resolver conflitos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,10 @@
 class ProductsController < ApplicationController
-<<<<<<<<< Temporary merge branch 1
+
     def show 
         @product = Product.find(params[:id])
     end
-=========
 
   def index
     @products = Product.all
   end
->>>>>>>>> Temporary merge branch 2
 end
