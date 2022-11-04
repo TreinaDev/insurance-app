227336d0bf4d5fed19e5148124e157703a71d4f5
manueladinizc <manuela.mdc@gmail.com>
2022-11-04 17:57:41 -0300

    ajustar erros rubocop
    Co-authored: Sarah <sarahsantosst@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,10 @@
 
 User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password', role: :admin)
 
-product_category_1 = ProductCategory.create!(name: "Celular")
-product_category_2 = ProductCategory.create!(name: 'TV')
+product_category_a = ProductCategory.create!(name: 'Celular')
+product_category_b = ProductCategory.create!(name: 'TV')
 
-Product.create!(product_model: "Samsung Galaxy S20", launch_year: "2018", brand: "Samsung", price: 2000.0, product_category: product_category_1)
-Product.create!(product_model: 'TV 32', launch_year: '2022', brand: 'LG', price: 5000, product_category: product_category_2)
+Product.create!(product_model: 'Samsung Galaxy S20', launch_year: '2018', brand: 'Samsung', price: 2000.0,
+                product_category: product_category_a)
+Product.create!(product_model: 'TV 32', launch_year: '2022', brand: 'LG', price: 5000,
+                product_category: product_category_b)
