eef730921d69140167b0f3c7315e8f10b4635d8c
João Almeida <joao@campuscode.com.br>
2022-11-07 17:16:22 -0300

    Resolve conflito em teste de visualização de produtos

--- spec/system/product/user_view_product_list_spec.rb
+++ spec/system/product/user_view_product_list_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 describe 'Administrador vê lista de produtos' do
   it 'com sucesso' do
-<<<<<<< HEAD
-    product_category = ProductCategory.create!(name: 'TV')
-    Product.create!(product_model: 'TV 32', launch_year: '2022', brand: 'LG',
-                    price: 5000, product_category: product_category)
-    Product.create!(product_model: 'TV 50', launch_year: '2021', brand: 'SAMSUNG',
-                    price: 8000, product_category: product_category)
-
-=======
     user = User.create!(name: 'Aline', email: 'Aline@empresa.com.br', password: 'password', role: :admin)
     product_category = ProductCategory.create!(name: 'TV')
     Product.create!(product_model: 'TV 32', launch_year: '2022', brand: 'LG', price: 5000,
@@ -18,7 +10,6 @@ describe 'Administrador vê lista de produtos' do
                     product_category:)
 
     login_as(user)
->>>>>>> 015d2e7a4ba200fde2c10e3586e281d25e25c687
     visit products_path
 
     expect(page).to have_content 'TV 32'
