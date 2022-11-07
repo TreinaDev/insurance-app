a46061b7d33ba6d4601bbc9ab2c7aa42a983309e
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-11-07 17:18:18 -0300

    Resolução de conflito no seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
 InsuranceCompany.create!(name: 'Empresa', email_domain: 'empresa.com.br')
-User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password', role: :admin)
-=======
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password', role: :admin)
 
 product_category_a = ProductCategory.create!(name: 'Celular')
@@ -19,4 +8,3 @@ Product.create!(product_model: 'Samsung Galaxy S20', launch_year: '2018', brand:
                 product_category: product_category_a)
 Product.create!(product_model: 'TV 32', launch_year: '2022', brand: 'LG', price: 5000,
                 product_category: product_category_b)
->>>>>>> main
