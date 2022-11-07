2ad646ac760aab0fdf2edb357d985c9433ddfab4
manueladinizc <manuela.mdc@gmail.com>
2022-11-07 19:08:50 -0300

    listar seguradoras

--- spec/system/insurance/user_view_isurance_list_spec.rb
+++ spec/system/insurance/user_view_isurance_list_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'Usuário vê lista de seguradoras' do
   it 'a partir da tela inicial' do
-    user = User.create!(name: 'Aline', email: 'Aline@empresa.com.br', password: 'password', role: :admin)
+    
     InsuranceCompany.create!(name: 'Porto Seguro', email_domain: 'portoseguro.com.br')
+    user = User.create!(name: 'Aline', email: 'Aline@portoseguro.com.br', password: 'password', role: :admin)
 
     login_as(user)
     visit root_path
