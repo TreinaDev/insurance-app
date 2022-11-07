585aab5d232245b85a091a21ee57f32e210171fe
Mariana S <marianaletii@gmail.com>
2022-11-07 20:07:41 -0300

    Alterações de qualidade de código

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  def check_if_admin
-    redirect_to root_path, notice: t('.authorization_failure') if current_user.employee?
-  end
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def check_if_admin
+    redirect_to root_path, notice: t('.authorization_failure') if current_user.employee?
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
