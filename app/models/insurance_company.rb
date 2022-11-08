be95f9805c5b74f34bec418ef69dd99259bc0ac6
manueladinizc <manuela.mdc@gmail.com>
2022-11-08 13:52:23 -0300

    resoluçao de conflitos

--- app/models/insurance_company.rb
+++ app/models/insurance_company.rb
@@ -1,6 +1,9 @@
 class InsuranceCompany < ApplicationRecord
   enum company_status: { active: 0, inactive: 1 }
   validates :registration_number, length: { is: 14 }
+  before_validation :generate_token, on: :create
+  validates :token, length: { is: 20 }
+  validates :token, uniqueness: true
 
   private
 
--- spec/system/insurance_company/user_view_isurance_list_spec.rb
+++ spec/system/insurance_company/user_view_isurance_list_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe 'Usuário vê lista de seguradoras' do
   it 'a partir da tela inicial' do
-    InsuranceCompany.create!(name: 'Allianz Seguros', email_domain: 'allianzaeguros.com.br', company_status: 1,
-                             registration_number: '84157841000105')
+    InsuranceCompany.create!(name: 'Allianz Seguros', email_domain: 'allianzaeguros.com.br',
+                             company_status: 1, registration_number: '84157841000105')
     InsuranceCompany.create!(name: 'Porto Seguro', email_domain: 'portoseguro.com.br',
                              registration_number: '99157841000105')
-    user = User.create!(name: 'Aline', email: 'Aline@portoseguro.com.br', password: 'password', role: :admin)
+
+    InsuranceCompany.create!(name: 'Empresa', email_domain: 'empresa.com.br', registration_number: '44639834000117')
+    user = User.create!(name: 'Aline', email: 'Aline@empresa.com.br', password: 'password', role: :admin)
 
     login_as(user)
     visit root_path
@@ -23,7 +25,7 @@ describe 'Usuário vê lista de seguradoras' do
     expect(page).to have_content '84157841000105'
   end
 
-  it 'e não possui acesso a essa página' do
+  it 'e não tem nenhuma seguradora' do
     InsuranceCompany.create!(name: 'Empresa', email_domain: 'empresa.com.br', registration_number: '44639834000117')
     user = User.create!(name: 'Aline', email: 'Aline@empresa.com.br', password: 'password', role: :admin)
 
@@ -34,7 +36,7 @@ describe 'Usuário vê lista de seguradoras' do
     expect(page).to have_content 'Não existem seguradoras cadastradas'
   end
 
-  it 'e não tem nenhuma seguradora' do
+  it 'funcionário não possui acesso a essa página' do
     InsuranceCompany.create!(name: 'Allianz Seguros', email_domain: 'allianzaeguros.com.br',
                              registration_number: '44639834000117')
     user = User.create!(name: 'Aline', email: 'Aline@allianzaeguros.com.br', password: 'password', role: :employee)
