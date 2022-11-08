7093a1c4cd6235bbc7ed3b5ef90eeae01df2b5b0
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-11-08 09:33:30 -0300

    Formatação da tela de listagem de produtos

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @products = Product.all
+    @products = Product.all.order(:product_category)
   end
 
   def show
--- spec/system/product/user_view_product_list_spec.rb
+++ spec/system/product/user_view_product_list_spec.rb
@@ -6,13 +6,22 @@ describe 'Administrador vê lista de produtos' do
     user = User.create!(name: 'Aline', email: 'Aline@empresa.com.br', password: 'password', role: :admin)
     product_category = ProductCategory.create!(name: 'TV')
     Product.create!(product_model: 'TV 32', launch_year: '2022', brand: 'LG', price: 5000,
-                    product_category:)
+                    product_category:, status: :active)
     Product.create!(product_model: 'TV 50', launch_year: '2021', brand: 'SAMSUNG', price: 8000,
-                    product_category:)
+                    product_category:, status: :inactive)
 
     login_as(user)
     visit products_path
 
+    expect(page).to have_content 'Categoria'
+    expect(page).to have_content 'Marca'
+    expect(page).to have_content 'Modelo'
+    expect(page).to have_content 'Ano de Lançamento'
+    expect(page).to have_content '2022'
+    expect(page).to have_content '2021'
+    expect(page).to have_content 'Status'
+    expect(page).to have_content 'Ativo'
+    expect(page).to have_content 'Inativo'
     expect(page).to have_content 'TV 32'
     expect(page).to have_content 'LG'
     expect(page).to have_content 'TV 50'
