e6ec5275e551da90c66f940acb68abf17a93ca48
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-11-08 13:41:34 -0300

    Ajuste do rspec e rubocop após nova PR aprovada

--- spec/system/product/user_view_product_list_spec.rb
+++ spec/system/product/user_view_product_list_spec.rb
@@ -34,17 +34,28 @@ describe 'Administrador vê lista de produtos' do
     user = User.create!(name: 'Edna', email: 'edna@empresa.com.br', password: 'password', role: :employee)
     product_category = ProductCategory.create!(name: 'TV')
     Product.create!(product_model: 'TV 32', launch_year: '2022', brand: 'LG',
-                    price: 5000, product_category_id: product_category.id)
+                    price: 5000, product_category_id: product_category.id, status: :active)
     Product.create!(product_model: 'TV 50', launch_year: '2021', brand: 'SAMSUNG',
-                    price: 8000, product_category_id: product_category.id)
+                    price: 8000, product_category_id: product_category.id, status: :inactive)
 
     login_as(user)
     visit root_path
     click_on 'Produtos'
 
     expect(page).to have_content 'Lista de Produtos'
-    expect(page).to have_content 'Modelo do Produto: TV 32 - LG'
-    expect(page).to have_content 'Modelo do Produto: TV 50 - SAMSUNG'
+    expect(page).to have_content 'Categoria'
+    expect(page).to have_content 'Marca'
+    expect(page).to have_content 'Modelo'
+    expect(page).to have_content 'Ano de Lançamento'
+    expect(page).to have_content '2022'
+    expect(page).to have_content '2021'
+    expect(page).to have_content 'Status'
+    expect(page).to have_content 'Ativo'
+    expect(page).to have_content 'Inativo'
+    expect(page).to have_content 'TV 32'
+    expect(page).to have_content 'LG'
+    expect(page).to have_content 'TV 50'
+    expect(page).to have_content 'SAMSUNG'
   end
 
   it 'e não tem nenhum produto cadastrado' do
