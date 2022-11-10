211977d4d1ef148aa90485b2b662e3c76726b6c3
Anita Luri <anitaluri.h@gmail.com>
2022-11-10 18:08:15 -0300

    Verificando autenticação do usuário

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,7 +2,8 @@ class PackagesController < ApplicationController
   def index
     if current_user.admin?
       @packages = Package.all.order(:product_category)
+    else
+      @packages = Package.where("insurance_company_id = ?", current_user.insurance_company_id)
     end
-    @packages = Package.where("insurance_company_id = ?", current_user.insurance_company_id)
   end
 end
