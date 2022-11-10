20cffe8c7fd7502731b9bcac6441bed9e6f1ca0a
Anita Luri <anitaluri.h@gmail.com>
2022-11-10 20:16:29 -0300

    Teste request

---
+++ spec/requests/user_view_package_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Usuário visualiza pacotes' do
+  it 'e não está logado' do
+    get(packages_path)
+
+    expect(response).to redirect_to(user_session_path)
+  end
+end
