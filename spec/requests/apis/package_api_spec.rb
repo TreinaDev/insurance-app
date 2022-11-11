5b0abbc17e366e36932fff8824834f1a2b10298f
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-11-11 16:37:22 -0300

    Ajustes no rubocop
    Co-authored-by: Sarah <sarahsantosst@gmail.com>

--- spec/requests/apis/package_api_spec.rb
+++ spec/requests/apis/package_api_spec.rb
@@ -32,7 +32,7 @@ describe 'Package API' do
       expect(json_response).to eq []
     end
 
-    it 'e retorna erro interno' do 
+    it 'e retorna erro interno' do
       allow(Package).to receive(:all).and_raise(ActiveRecord::QueryAborted)
 
       get '/api/v1/packages'
@@ -40,7 +40,7 @@ describe 'Package API' do
       expect(response).to have_http_status 500
     end
   end
-  
+
   context 'GET /api/v1/packages/1' do
     it 'sucesso' do
       product_category = ProductCategory.create!(name: 'Televisão')
