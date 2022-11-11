fee870dd55500cf227f4c343b3df87fe85bad851
Anita Luri <anitaluri.h@gmail.com>
2022-11-11 15:47:46 -0300

    Usuário vê lista de pacotes pendentes

--- spec/system/pending_package/user_view_pending_package_spec.rb
+++ spec/system/pending_package/user_view_pending_package_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-describe 'Usuário vê lista de pacotes' do
+describe 'Usuário vê lista de pacotes pendentes' do
   it 'com sucesso e é administrador' do
     user = User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password',
                         role: :admin)
-    insurance = InsuranceCompany.create!(name: 'Seguradora', email_domain: 'seguradora.com.br', registration_number:
-                                        '01000000123410')
-    smartphones = ProductCategory.create!(name: 'Smartphones')
-    PendingPackage.create!(name: 'Premium', min_period: 12, max_period: 24, insurance_company: insurance,
-                          product_category: smartphones)
-    PendingPackage.create!(name: 'Econômico', min_period: 6, max_period: 18, insurance_company: insurance,
-                          product_category: smartphones)
+    c = InsuranceCompany.create!(name: 'Seguradora', email_domain: 'seguradora.com.br', registration_number:
+                                      '01000000123410')
+    category = ProductCategory.create!(name: 'Smartphones')
+    PendingPackage.create!(name: 'Premium', min_period: 12, max_period: 24, insurance_company: c, product_category:
+                          category)
+    PendingPackage.create!(name: 'Econômico', min_period: 6, max_period: 18, insurance_company: c, product_category:
+                          category)
 
     login_as(user)
     visit root_path
     within('nav') do
-      click_on 'Pacotes Pendetes'
+      click_on 'Pacotes Pendentes'
     end
 
     expect(page).to have_content('Lista de Pacotes Pendentes')
@@ -35,5 +35,51 @@ describe 'Usuário vê lista de pacotes' do
     expect(page).to have_content('Econômico')
     expect(page).to have_content('6 meses')
     expect(page).to have_content('18 meses')
+  end
+
+  it 'e não tem nenhum pacote pendente cadastrado' do
+    user = User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password',
+                        role: :admin)
+
+    login_as(user)
+    visit root_path
+    within('nav') do
+      click_on 'Pacotes Pendentes'
+    end
+
+    expect(page).to have_content('Não existem pacotes pendentes cadastrados')
+  end
+
+  it 'com sucesso e é funcionário de uma seguradora' do
+    insurance_a = InsuranceCompany.create!(name: 'Azul', email_domain: 'azul.com.br', registration_number:
+                                          '01000000123410')
+    insurance_b = InsuranceCompany.create!(name: 'Laranja', email_domain: 'laranja.com.br', registration_number:
+                                          '02000000258210')
+    user = User.create!(name: 'Funcionario', email: 'pessoa@azul.com.br', password: 'password',
+                        role: :employee, insurance_company: insurance_a)
+    smartphones = ProductCategory.create!(name: 'Smartphones')
+    laptops = ProductCategory.create!(name: 'Laptops')
+    PendingPackage.create!(name: 'Premium', min_period: 12, max_period: 24, insurance_company: insurance_a,
+                          product_category: smartphones)
+    PendingPackage.create!(name: 'Premium', min_period: 12, max_period: 24, insurance_company: insurance_b,
+                          product_category: laptops)
+
+    login_as(user)
+    visit root_path
+    within('nav') do
+      click_on 'Pacotes Pendentes'
+    end
+
+    expect(page).to have_link('Azul')
+    expect(page).to have_content('Premium')
+    expect(page).to have_content('12 meses')
+    expect(page).to have_content('24 meses')
+    expect(page).to have_content('Smartphones')
+    expect(page).to have_content('Pendente')
+    expect(page).not_to have_link('Laranja')
+    expect(page).not_to have_content('Econômico')
+    expect(page).not_to have_content('6 meses')
+    expect(page).not_to have_content('18 meses')
+    expect(page).not_to have_content('Laptops')
   end
 end
