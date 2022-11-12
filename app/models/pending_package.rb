6f68fc733a56bb9f0bd1796c083574345117c32f
Anita Luri <anitaluri.h@gmail.com>
2022-11-11 23:40:00 -0300

    Testes unitários

--- app/models/pending_package.rb
+++ app/models/pending_package.rb
@@ -4,7 +4,7 @@ class PendingPackage < ApplicationRecord
 
   validates :name, :min_period, :max_period, presence: true
   validates :min_period, numericality: { greater_than: 0 }
-  validates :min_period, comparison: { less_than: :max_period }
+  validates :min_period, comparison: { less_than_or_equal_to: :max_period }
 
   enum status: { pending: 0, active: 9 }
 end
