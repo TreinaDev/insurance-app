60a790d7b73b75839e30ed1d7a5f1d99f1ce0916
Anita Luri <anitaluri.h@gmail.com>
2022-11-14 12:18:53 -0300

    Testes unitários

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,4 +1,35 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Service, type: :model do
-# end
+RSpec.describe Service, type: :model do
+  describe '#valid?' do
+    it 'falso quando o nome fica em branco' do
+      service = Service.new(name: '',
+                            description: 'Concede 10% de desconto em assinatura com mais canais disponíveis no mercado.')
+
+      expect(service.valid?).to eq false
+    end
+
+    it 'falso quando a descrição fica em branco' do
+      service = Service.new(name: 'Assinatura TV',
+                            description: '')
+
+      expect(service.valid?).to eq false
+    end
+
+    it 'falso quando o nome já está em uso' do
+      Service.create!(name: 'Assinatura TV',
+                      description: 'Concede um mês grátis em assinatura do netflix.')
+      service = Service.new(name: 'Assinatura TV',
+                            description: 'Concede 10% de desconto em assinatura com mais canais disponíveis no mercado.')
+
+      expect(service.valid?).to eq false
+    end
+
+    it 'falso quando a descrição for menor que 3 caracteres' do
+      service = Service.new(name: 'Assinatura TV',
+                            description: 'Co')
+
+      expect(service.valid?).to eq false
+    end
+  end
+end
