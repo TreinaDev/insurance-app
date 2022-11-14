cace20c281b50b4a01ad9cb37ad215658c62d4ce
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-11-14 19:02:29 -0300

    Formatação da tela de cadastro de categoria de produto
    Co-authored-by: Sarah <sarahsantosst@gmail.com>

--- spec/system/product_category/admin_register_product_category_spec.rb
+++ spec/system/product_category/admin_register_product_category_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 describe 'Administrador cadastra categoria de produto' do
+  it 'a partir da lista de categorias' do
+    admin = User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password', role: :admin)
+    ProductCategory.create!(name: 'Celular')
+    ProductCategory.create!(name: 'Desktop')
+    ProductCategory.create!(name: 'Tablet')
+
+    login_as(admin)
+    visit root_path
+    click_on 'Categorias de Produto'
+    click_on 'Cadastrar Categoria'
+
+    expect(page).to have_content 'Cadastrar Categoria de Produto'
+    within 'main' do
+      expect(page).to have_content 'Categorias de Produto'
+    end
+    expect(page).to have_content 'Celular'
+    expect(page).to have_content 'Desktop'
+    expect(page).to have_content 'Tablet'
+  end
+
   it 'com sucesso' do
     admin = User.create!(name: 'Pessoa', email: 'pessoa@empresa.com.br', password: 'password', role: :admin)
 
