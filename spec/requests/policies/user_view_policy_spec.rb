1c05835c7d749a159d2d7d009ed177d88ab3cb44
manueladinizc <manuela.mdc@gmail.com>
2022-11-18 18:41:00 -0300

    correção do rubocop

--- spec/requests/policies/user_view_policy_spec.rb
+++ spec/requests/policies/user_view_policy_spec.rb
@@ -26,11 +26,11 @@ RSpec.describe 'Usuário visualiza lista de apólices', type: :request do
     product_category = ProductCategory.create!(name: 'TV')
     package = Package.create!(name: 'Premium', min_period: 12, max_period: 24,
                               insurance_company:, price: 90.00, product_category_id: product_category.id)
-    policy = Policy.create(client_name: 'Maria Alves', client_registration_number: '99950033340',
-                           client_email: 'mariaalves@email.com',
-                           insurance_company_id: insurance_company.id, order_id: 1,
-                           equipment_id: 1, purchase_date: Time.zone.today,
-                           policy_period: 12, package_id: package.id, status: :active)
+    Policy.create(client_name: 'Maria Alves', client_registration_number: '99950033340',
+                  client_email: 'mariaalves@email.com',
+                  insurance_company_id: insurance_company.id, order_id: 1,
+                  equipment_id: 1, purchase_date: Time.zone.today,
+                  policy_period: 12, package_id: package.id, status: :active)
 
     get '/policies'
 
