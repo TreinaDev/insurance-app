4a45a11736fdbb234a10236feacba61557f4ecd6
manueladinizc <manuela.mdc@gmail.com>
2022-11-20 19:04:45 -0300

    teste para admin

--- spec/system/policy/user_changes_policy_status_spec.rb
+++ spec/system/policy/user_changes_policy_status_spec.rb
@@ -57,4 +57,34 @@ describe 'Usuário altera status da apólice' do
       expect(page).to have_content 'Situação da Apólice: Cancelada'
     end
   end
+
+  context 'como administrador' do
+    it 'e não vê botão de alterar status' do
+      user = User.create!(email: 'maria@ligaseguradora.com.br', password: 'password', name: 'Maria', role: :admin)
+      insurance_company = InsuranceCompany.create!(name: 'Liga Seguradora', email_domain: 'ligaseguradora.com.br',
+                                                   registration_number: '84157841000105')
+      product_category = ProductCategory.create!(name: 'TV')
+
+      package = Package.create!(name: 'Premium', min_period: 12, max_period: 24,
+                                insurance_company_id: insurance_company.id,
+                                price: 90.00, product_category_id: product_category.id)
+
+      allow(SecureRandom).to receive(:alphanumeric).with(10).and_return('GHI1234567')
+      Policy.create(client_name: 'Maria Alves', client_registration_number: '99950033340',
+                    client_email: 'mariaalves@email.com',
+                    insurance_company_id: insurance_company.id, order_id: 1,
+                    equipment_id: 1, purchase_date: Time.zone.today,
+                    policy_period: 12, package_id: package.id, status: :pending)
+
+      login_as(user)
+      visit root_path
+      click_on 'Apólices'
+      click_on 'Pendentes'
+      find(:css, '#pending-tab-pane').click_on 'GHI1234567'
+
+      expect(page).not_to have_content 'Aprovar'
+      expect(page).not_to have_content 'Reprovar'
+      expect(page).to have_content 'Apólice: GHI1234567'
+    end
+  end
 end
