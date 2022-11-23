8a2e63eefc20e96dda965650143b0de6f744baa2
manueladinizc <manuela.mdc@gmail.com>
2022-11-23 20:47:00 -0300

    teste request

--- spec/requests/user_view_services_spec.rb
+++ spec/requests/user_view_services_spec.rb
@@ -7,3 +7,14 @@ describe 'Usuário visualiza serviços' do
     expect(response).to redirect_to(user_session_path)
   end
 end
+
+describe 'Usuário acessar detalhes de serviço' do
+  it 'e não está logado' do
+    service = Service.create!(name: 'Assinatura TV',
+                              description: 'Concede 10% de desconto em assinatura.',
+                              status: :active, code: 'ZK7')
+    get "/package_coverages/#{service.id}"
+
+    expect(response).to redirect_to(user_session_path)
+  end
+end
