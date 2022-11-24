adc0aa3f3ac9908124231d0c280d915d48e0a6fa
Raphaelly V <valiengo.raphaelly@yahoo.com>
2022-11-24 12:53:13 -0300

    Erros ao procurar um pacote para um produto
    Co-authored-by: Anita <anitaluri.h@gmail.com>

--- app/controllers/api/v1/packages_controller.rb
+++ app/controllers/api/v1/packages_controller.rb
@@ -5,13 +5,22 @@ class Api::V1::PackagesController < Api::V1::ApiController
   end
 
   def show
+    @package = Package.find(params[:id])
     if params[:product_id].nil?
-      package = Package.find(params[:id])
-      render status: :ok, json: package.as_json(except: %i[created_at updated_at])
+      render status: :ok, json: @package.as_json(except: %i[created_at updated_at])
     else
-      product = Product.find(params[:product_id])
-      package = Package.find(params[:id])
-      render status: :ok, json: create_package_json(package, product)
+      @product = Product.find(params[:product_id])
+      api_show
+    end
+  end
+
+  private
+
+  def api_show
+    if @product.product_category == @package.product_category
+      render status: :ok, json: create_package_json(@package, @product)
+    else
+      return404
     end
   end
 end
